68cd5348622b2ce572b075dd383ea34865185d7f
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-12-05 19:56:41 +0300

    add if

---
+++ cmake-build-debug/CMakeFiles/module_4a.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/module_4a.dir/main.cpp.obj"
+  "CMakeFiles/module_4a.dir/main.cpp.obj.d"
+  "libmodule_4a.dll.a"
+  "module_4a.exe"
+  "module_4a.exe.manifest"
+  "module_4a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/module_4a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
